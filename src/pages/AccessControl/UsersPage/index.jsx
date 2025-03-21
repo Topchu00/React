5073ddad58e4ec62c0a65d9e608ce5903547f6ad
Topchu00 <orozbekovtopchu@gmail.com>
2025-03-21 20:57:32 +0600

    Axios

--- src/pages/AccessControl/UsersPage/index.jsx
+++ src/pages/AccessControl/UsersPage/index.jsx
@@ -1,8 +1,50 @@
+import { useEffect, useState } from "react"
+import { Dialog } from "primereact/dialog";
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { UsersService } from "../../../entities/users"
+
 export default function UsersPage() {
+    const {getUsers} = UsersService()
+
+    const [users, setUsers] = useState([])
+    const [selectedUser, setselectedUser] = useState(null)
+    const [isShowUserDetails, setisShowUserDetails] = useState(false)
+
+    const fetchUsers = async () => {
+        const arr = await getUsers()
+        console.log({arr});
+        setUsers([...arr])
+    }
+
+    const onSelectionChange = (evt) => {
+        setselectedUser(evt.value)
+        setisShowUserDetails(true)
+    }
+
+    useEffect(() => {
+        fetchUsers()
+
+        console.log(users);
+        
+    }, [])
+
+
+
     return (
-        <div>
-            <h1>Users Page</h1>
-        </div>
+        <>
+            <DataTable value={users} selectionMode='single' selection={selectedUser} onSelectionChange={onSelectionChange}>
+                <Column field="id" header="UserId"></Column>
+                <Column field="username" header="UserName"></Column>
+                <Column field="name" header="FullName"></Column>
+                <Column field="phone" header="UserPhone"></Column>
+                <Column field="email" header="UserEmail"></Column>
+                <Column field="website" header="UserWebSite"></Column>
+            </DataTable>
+
+            <Dialog visible={isShowUserDetails} onHide={() => setisShowUserDetails(false)}>
+                {JSON.stringify(selectedUser)}
+            </Dialog>
+        </>
     )
 }
-
--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,63 +1,7 @@
-import { Button } from 'primereact/button'
-import { useEffect, useState } from 'react'
 export default function MainPage() {
-
-    // Состояние в React - это объект который содержит некие переменные, влияющие на рендеринг компонента.Когда состояние меняется, React автоматически перерисовывает компонент или по другому ре-рендерит.
-
-    // Хуки - это функции, которые позволяют нам "подцепиться" к состоянию и методам жизненного цикла React из функционального компонента.
-
-    // useState - это хук в React который позволяет добавлять состояние в компонентах
-
-    // Жизненный цикл компоненты - это последовательность этапов, через которые проходит компонента React от его создания (монтирования) до его удаления (размонтирования) из DOM 
-
-    // Этапы жизненного цикла компоненты:
-    // 1. Mounting (монтирование)
-    // 2. Updating (обновление)
-    // 3. Unmounting (размонтирование)
-
-    // useEffect - это хук в React который позволяет выполнять побочные эффекты в функциональных компонентах.
-
-
-
-    const [value, setValue] = useState(2)
-    const [count, setCount] = useState(2)
-
-    useEffect(() => {
-        console.log('обновление компонента прошло успешно')
-
-    }, [count])
-
+    
     return (
         <div>
-            <p>
-                <b>{value === 0 ?
-                    'Минимальное значение' : value
-                }</b>
-            </p>
-            <div>
-                <Button label='Click -' onClick={() => {
-                    if (value > 0) {
-                        setValue(value - 1)
-                    }
-                }} />
-
-                <Button label='Click +' onClick={() => setValue(value + 1)} />
-            </div>
-
-            <p>
-                <b>{count === 0 ?
-                    'Минимальное значение' : count
-                }</b>
-            </p>
-            <div>
-                <Button label='Click -' onClick={() => {
-                    if (count > 0) {
-                        setCount(count - 1)
-                    }
-                }} />
-
-                <Button label='Click +' onClick={() => setCount(count + 1)} />
-            </div>
             <h1>Main Page</h1>
         </div>
     )
